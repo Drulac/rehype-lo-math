44a20728b065c6d2437433fffac299923f1bc852
drulac <drulac@protonmail.com>
2020-07-10 10:19:37 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # rehype-lo-math
 Transforms math nodes with LibreOffice (writer) math syntax (simplier than AsciiMath)
+
+## install
+
+```sh
+npm install --save rehype-lo-math
+```
+
+## usage
+
+same as [rehype-katex](https://www.npmjs.com/package/rehype-katex) or [rehype-mathjax](https://www.npmjs.com/package/rehype-mathjax) :
+
+Say we have the following file, `example.html`:
+
+```html
+<p>
+  Lift(<span class="math math-inline">L</span>) can be determined by Lift Coefficient
+  (<span class="math math-inline">C_L</span>) like the following equation.
+</p>
+
+<div class="math math-display">
+  L = {1} over {2} * rho v^2 S C_L
+</div>
+```
+
+And our script, `example.js`, looks as follows:
+
+```js
+const vfile = require('to-vfile')
+const unified = require('unified')
+const parse = require('rehype-parse')
+const mathjax = require('rehype-lo-math')
+const stringify = require('rehype-stringify')
+
+unified()
+  .use(parse, {fragment: true})
+  .use(mathjax)
+  .use(stringify)
+  .process(vfile.readSync('example.html'), function (err, file) {
+    if (err) throw err
+    console.log(String(file))
+  })
+```
+
+Now, run `node example` 
---
+++ index.js
@@ -0,0 +1,84 @@
+const visit = require('unist-util-visit')
+
+const unified = require('unified')
+const parse = require('rehype-parse')
+const toText = require('hast-util-to-text')
+
+const convertFormule = require('./convertFormule.js')
+const mathjaxProm = require('mathjax')
+	.init({
+		loader: { load: ['input/asciimath', 'output/svg'] },
+	})
+	.catch((err) => console.log(err.message))
+	.then((MathJax) => {
+		return (str, opts) => {
+			//TODO: add cache to avoid repetitive math calculs
+
+			try {
+				const svg = MathJax.asciimath2svg(str, {
+					display: true,
+				})
+				return MathJax.startup.adaptor.outerHTML(svg)
+			} catch (error) {
+				console.error(error)
+			}
+		}
+	})
+
+module.exports = rehypeKatex
+
+const parseHtml = unified().use(parse, {
+	fragment: true,
+	position: false,
+})
+
+const source = 'rehype-LO-math'
+
+function rehypeKatex(options) {
+	const opts = options || {}
+	const throwOnError = opts.throwOnError || false
+
+	return transformMath
+
+	async function transformMath(tree, file) {
+		const mathjax = await mathjaxProm
+
+		visit(tree, 'element', onelement)
+
+		function onelement(element) {
+			const classes = element.properties.className || []
+			const inline = classes.includes('math-inline')
+			const displayMode = classes.includes('math-display')
+
+			if (!inline && !displayMode) {
+				return
+			}
+
+			const value = toText(element)
+
+			let result
+
+			try {
+				console.log(value)
+				console.log(convertFormule(value))
+				result = `<span class="mathjax">${mathjax(
+					convertFormule(value)
+				)}</span>`
+
+				console.log(result)
+			} catch (error) {
+				const fn = throwOnError ? 'fail' : 'message'
+				const origin = [
+					source,
+					error.name.toLowerCase(),
+				].join(':')
+
+				file[fn](error.message, element.position, origin)
+
+				result = mathjax(value)
+			}
+
+			element.children = parseHtml.parse(result).children
+		}
+	}
+}
